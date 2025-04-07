1047776506e18e29c81e9a815eda999f60e5c12b
remi.trotel <remi.trotel@epita.fr>
2025-04-07 12:29:12 +0200

    doesn't work

--- takicine/src/app/app.component.ts
+++ takicine/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,12 @@ import { FooterComponent } from './footer/footer.component';
   imports: [NavbarComponent, HomeComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+})
+@NgModule({
+  imports: [
+  	BrowserAnimationsModule,
+  	ToastrModule.forRoot()
+  ],
 })
 export class AppComponent {
   title = 'takicine';
