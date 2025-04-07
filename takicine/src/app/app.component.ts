cef2d0157b7d2ad1b8ef575f363ba4cb51e00e2c
remi.trotel <remi.trotel@epita.fr>
2025-04-07 15:56:46 +0200

    Add link to user button in admin

--- takicine/src/app/app.component.ts
+++ takicine/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent, RouterOutlet, FooterComponent],
+  imports: [NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
